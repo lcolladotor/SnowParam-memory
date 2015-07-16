6ad9791dea13a46529cf8ca637b8ab9e7a8ab2d3
lcolladotor <lcollado@jhsph.edu>
2015-07-16 19:06:49 -0400

    Added a script for logging memory and submitting all jobs

---
+++ runTests.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+
+## Usage
+# sh runTests.sh
+
+## Following script based on
+## http://superuser.com/questions/297283/store-the-output-of-date-and-watch-command-to-a-file
+cat > .logMemory.sh <<EOF
+#!/bin/bash
+#$ -cwd
+#$ -m e
+
+touch logs/logMemory.txt
+while true
+do
+    qmem | tee -a logs/logMemory.txt
+    sleep 2
+done
+EOF
+
+echo "Submitting log memory job"
+qsub .logMemory.sh
+sleep 120
+
+sh SnowParam-memory.sh
+sh SnowParam-memory-derfinder.sh
